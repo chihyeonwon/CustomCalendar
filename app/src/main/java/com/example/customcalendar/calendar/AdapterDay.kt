3aa7b225e2f39c3fe8a7cc1c2d8cadd3efe184e6
anos1 <anos1021@gmail.com>
2023-10-10 22:34:34 +0900

    오늘 날짜 강조, 클릭 이펙트 추가

--- app/src/main/java/com/example/customcalendar/calendar/AdapterDay.kt
+++ app/src/main/java/com/example/customcalendar/calendar/AdapterDay.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.example.customcalendar.R
 import com.example.customcalendar.databinding.DayAdapterBinding
+import java.text.DateFormat
+import java.text.SimpleDateFormat
 import java.util.*
 
 class AdapterDay(val tempMonth:Int, val dayList: MutableList<Date>): RecyclerView.Adapter<AdapterDay.DayView>() {
@@ -19,10 +22,15 @@ class AdapterDay(val tempMonth:Int, val dayList: MutableList<Date>): RecyclerVie
     }
 
     override fun onBindViewHolder(holder: DayView, position: Int) {
-        holder.binding.itemDayLayout.setOnClickListener {
-            Toast.makeText(holder.binding.root.context, "${dayList[position]}", Toast.LENGTH_SHORT).show()
-        }
+
+        val select = SimpleDateFormat("yyyy MM dd")
+
         holder.binding.itemDayText.text = dayList[position].date.toString()
+
+        holder.binding.itemDayLayout.setOnClickListener {
+            //Toast.makeText(holder.binding.root.context, "${dayList[position]}", Toast.LENGTH_SHORT).show()
+            Toast.makeText(holder.binding.root.context, "${select.format(dayList[position])}", Toast.LENGTH_SHORT).show()
+        }
 
         holder.binding.itemDayText.setTextColor(when(position % 7) {
             0 -> Color.RED
@@ -30,10 +38,13 @@ class AdapterDay(val tempMonth:Int, val dayList: MutableList<Date>): RecyclerVie
             else -> Color.BLACK
         })
 
-        if(tempMonth != dayList[position].month) {
+        if(tempMonth != dayList[position].month) { // 해당 월 외의 날짜 투명도
             holder.binding.itemDayText.alpha = 0.4f
         }
 
+        if(Calendar.getInstance().get(Calendar.MONTH) == dayList[position].month && Calendar.getInstance().get(Calendar.DATE) == dayList[position].date) { // 오늘 날짜 강조
+            holder.binding.itemDayLayout.setBackgroundResource(R.drawable.round_border)
+        }
     }
 
     override fun getItemCount(): Int {
