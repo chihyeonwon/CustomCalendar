5ebd2a358e4a53e73a6410a8daad4b324761d399
anos1 <anos1021@gmail.com>
2023-11-26 20:10:18 +0900

    일정 공유 활성화 버튼 비활성화 검색 구조 개선

--- app/src/main/java/com/example/customcalendar/calendar/AdapterDay.kt
+++ app/src/main/java/com/example/customcalendar/calendar/AdapterDay.kt
@@ -153,12 +153,9 @@ class AdapterDay(val tempMonth:Int, val dayList: MutableList<Date>, val height:I
             Log.i(item.myEmail, "세팅2")
         }
 
-        Timer().schedule(2000){
+        Timer().schedule(2000){ //딜레이
             getPlanData(holder, position, planData, planAdapter) // 달력 셀에 데이터 가져오기
         }
-
-
-
     }
 
     fun getDow(index: Int): String? {
